7aa80d2c48ed492b87164e43f0ce3b25d03b5a6d
Sandeep.M <sandeep.m24.rathnam@gmail.com>
2021-10-15 19:40:23 +0530

    Added Auth Conditiong Rendering and Styling

--- src/Shared/Components/Navigation/NavLinks.js
+++ src/Shared/Components/Navigation/NavLinks.js
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useContext } from "react";
 import { NavLink } from "react-router-dom";
+import { AuthContext } from "../../Context/authContext";
 import "./NavLinks.css";
 
 const NavLinks = () => {
+  const auth = useContext(AuthContext);
   return (
     <ul className="nav-links">
       <li>
@@ -10,19 +12,26 @@ const NavLinks = () => {
           HOME
         </NavLink>
       </li>
-      <li>
-        <NavLink to="/MyBlogs">
-          MY BLOGS
-        </NavLink>
-      </li>
-      <li>
-        <NavLink to="/NewBlog">
-          NEW BLOG
-        </NavLink>
-      </li>
-      <li>
-        <NavLink to="/Login">LOGIN</NavLink>
-      </li>
+      {auth.isLoggedIn && (
+        <li>
+          <NavLink to="/MyBlogs">MY BLOGS</NavLink>
+        </li>
+      )}
+      {auth.isLoggedIn && (
+        <li>
+          <NavLink to="/NewBlog">NEW BLOG</NavLink>
+        </li>
+      )}
+      {!auth.isLoggedIn && (
+        <li>
+          <NavLink to="/Login">LOGIN</NavLink>
+        </li>
+      )}
+      {auth.isLoggedIn && (
+        <li>
+          <button onClick={auth.logout}>LOGOUT</button>
+        </li>
+      )}
     </ul>
   );
 };
