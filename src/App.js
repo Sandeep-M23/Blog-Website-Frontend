222cd21e0de2bfa5291cf0c4074912cad65a93b3
Sandeep.M <sandeep.m24.rathnam@gmail.com>
2021-10-15 19:42:08 +0530

    Added AuthContext Provider

--- src/App.js
+++ src/App.js
@@ -1,49 +1,90 @@
-import React from 'react';
-import {BrowserRouter as Router, Redirect, Route, Switch} from 'react-router-dom';
-import Layout from './hoc/Layout/Layout';
+import React, { useState, useCallback } from "react";
+import {
+  BrowserRouter as Router,
+  Redirect,
+  Route,
+  Switch,
+} from "react-router-dom";
+import Layout from "./hoc/Layout/Layout";
 
-import Home from './Home/Pages/Home';
-import NewBlog from './Blogs/Pages/NewBlog';
-import MyBlogs from './Blogs/Pages/MyBlogs';
-import UpdateBlog from './Blogs/Pages/UpdateBlog';
-import Info from './Info/Pages/Info';
-import Login from './Auth/Pages/Login';
+import Home from "./Home/Pages/Home";
+import NewBlog from "./Blogs/Pages/NewBlog";
+import MyBlogs from "./Blogs/Pages/MyBlogs";
+import UpdateBlog from "./Blogs/Pages/UpdateBlog";
+import Info from "./Info/Pages/Info";
+import Login from "./Auth/Pages/Login";
+
+import { AuthContext } from "./Shared/Context/authContext";
 
 const App = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [userId, setUserId] = useState(false);
 
-  const routes = (
-    <Switch>
-      <Route path="/" exact>
-        <Home/>
-      </Route>
-      <Route path="/Info" exact>
-        <Info/>
-      </Route>
-      <Route path="/NewBlog" exact>
-        <NewBlog/>
-      </Route>
-      <Route path="/MyBlogs" exact>
-        <MyBlogs/>
-      </Route>
-      <Route path="/UpdateBlog" exact>
-        <UpdateBlog/>
-      </Route>
-      <Route path="/Login" exact>
-        <Login/>
-      </Route>
-      <Redirect to="/"/>
-    </Switch>
-  )
+  const login = useCallback((uid) => {
+    setIsLoggedIn(true);
+    setUserId(uid);
+  }, []);
+
+  const logout = useCallback(() => {
+    setIsLoggedIn(false);
+    setUserId(null);
+  }, []);
+
+  let routes;
+  if (isLoggedIn) {
+    routes = (
+      <Switch>
+        <Route path="/" exact>
+          <Home />
+        </Route>
+        <Route path="/Info" exact>
+          <Info />
+        </Route>
+        <Route path="/NewBlog" exact>
+          <NewBlog />
+        </Route>
+        <Route path="/MyBlogs" exact>
+          <MyBlogs />
+        </Route>
+        <Route path="/UpdateBlog" exact>
+          <UpdateBlog />
+        </Route>
+        <Redirect to="/" />
+      </Switch>
+    );
+  } else {
+    routes = (
+      <Switch>
+        <Route path="/" exact>
+          <Home />
+        </Route>
+        <Route path="/Info" exact>
+          <Info />
+        </Route>
+        <Route path="/Login">
+          <Login />
+        </Route>
+        <Redirect to="/Login" />
+      </Switch>
+    );
+  }
 
   return (
     <div className="App">
-      <Router>
-      <Layout>
-        {routes}
-      </Layout>
-      </Router>
+      <AuthContext.Provider
+        value={{
+          isLoggedIn: isLoggedIn,
+          userId: userId,
+          login: login,
+          logout: logout,
+        }}
+      >
+        <Router>
+          <Layout>{routes}</Layout>
+        </Router>
+      </AuthContext.Provider>
     </div>
   );
-}
+};
 
 export default App;
