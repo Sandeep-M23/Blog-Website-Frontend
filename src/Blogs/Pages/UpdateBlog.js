0643c7c2176601a9d39006ed3c215442aa88c0b9
Sandeep.M <sandeep.m24.rathnam@gmail.com>
2021-10-16 19:26:18 +0530

    Added UpdateBlog Logic

--- src/Blogs/Pages/UpdateBlog.js
+++ src/Blogs/Pages/UpdateBlog.js
@@ -1,5 +1,5 @@
-import React, { useEffect, useState,useContext} from "react";
-import { useParams,useHistory} from "react-router-dom";
+import React, { useEffect, useState, useContext } from "react";
+import { useParams, useHistory } from "react-router-dom";
 
 import Input from "../../Shared/Components/Form-Elements/Input";
 import {
@@ -9,33 +9,100 @@ import {
 import Button from "../../Shared/Components/Form-Elements/Button";
 import Card from "../../Shared/Components/UI-Elements/Card";
 import { useForm } from "../../Shared/Hooks/Form-Hooks";
+import { useHttpClient } from "../../Shared/Hooks/Http-Hooks";
+import Spinner from "../../Shared/Components/UI-Elements/Spinner";
+import ErrorModal from "../../Shared/Components/UI-Elements/ErrorModal";
+import {AuthContext} from '../../Shared/Context/authContext';
 import "./BlogForm.css";
 
 const UpdateBlog = () => {
-  const [loadedPlace, setLoadedPlace] = useState();
-  const placeId = useParams().placeId;
+  const [loadedBlog, setLoadedBlog] = useState();
+  const { loading, error, sendRequest, clearError } = useHttpClient();
+  const blogId = useParams().blogId;
+  const auth = useContext(AuthContext);
   const history = useHistory();
 
   const [formState, inputHandler, setFormData] = useForm(
     {
       title: {
-        value: '',
-        isValid: false
+        value: "",
+        isValid: false,
       },
       description: {
-        value: '',
-        isValid: false
-      }
+        value: "",
+        isValid: false,
+      },
     },
     false
   );
 
-;
+  useEffect(() => {
+    console.log(blogId)
+    const fetchBlog = async () => {
+      try {
+        const responseData = await sendRequest(
+          `http://localhost:5000/blogs/${blogId}`
+        );
+        setLoadedBlog(responseData.blog);
+        setFormData(
+          {
+            title: {
+              value: responseData.place.title,
+              isValid: true,
+            },
+            description: {
+              value: responseData.place.description,
+              isValid: true,
+            },
+          },
+          true
+        );
+      } catch (err) {}
+    };
+    fetchBlog();
+  }, [sendRequest, blogId, setFormData]);
+
+  const updateBlogHandler = async(event) => {
+    event.preventDefault();
+    try{
+      await sendRequest(
+        `http://localhost:5000/blogs/${blogId}`,
+        "PATCH",
+        JSON.stringify({
+          title: formState.inputs.title.value,
+          description: formState.inputs.description.value,
+        }),
+        {
+          'Content-Type':'application/json'
+        }
+      );
+      history.push(`/${auth.userId}/blogs`)
+    }catch(err){}
+  }
+
+  if (loading) {
+    return (
+      <div className="center">
+        <Spinner />
+      </div>
+    );
+  }
+
+  if (!loadedBlog && !error) {
+    return (
+      <div className="center">
+        <Card className="empty_Card">
+          <h2>Could not find Blog!</h2>
+        </Card>
+      </div>
+    );
+  }
 
   return (
     <React.Fragment>
-      {(
-        <form className="blog-form">
+      <ErrorModal error={error} onClear={clearError} />
+      {!loading && loadedBlog && (
+        <form className="blog-form" onSubmit={updateBlogHandler}>
           <Input
             id="title"
             element="input"
@@ -44,7 +111,6 @@ const UpdateBlog = () => {
             validators={[VALIDATOR_REQUIRE()]}
             errorText="Please enter a valid title."
             onInput={inputHandler}
-            // initialValue={loadedPlace.title}
             initialValid={true}
           />
           <Input
@@ -55,11 +121,10 @@ const UpdateBlog = () => {
             validators={[VALIDATOR_MINLENGTH(5)]}
             errorText="Please enter a valid description (min. 5 characters)."
             onInput={inputHandler}
-            // initialValue={loadedPlace.description}
             initialValid={true}
           />
           <Button type="submit" disabled={!formState.isValid}>
-            UPDATE PLACE
+            UPDATE BLOG
           </Button>
         </form>
       )}
