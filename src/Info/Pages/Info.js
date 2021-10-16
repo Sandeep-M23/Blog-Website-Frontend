2294bffc269d40b96ab1250c457aab5b43e01469
Sandeep.M <sandeep.m24.rathnam@gmail.com>
2021-10-16 19:26:58 +0530

    Added InfoPage Logic

--- src/Info/Pages/Info.js
+++ src/Info/Pages/Info.js
@@ -1,13 +1,37 @@
-import React from 'react';
-
-import InfoCard from '../Components/InfoCard';
+import React, { useState, useEffect } from "react";
+import Spinner from "../../Shared/Components/UI-Elements/Spinner";
+import ErrorModal from "../../Shared/Components/UI-Elements/ErrorModal";
+import { useHttpClient } from "../../Shared/Hooks/Http-Hooks";
+import InfoCard from "../Components/InfoCard";
+import { useParams } from "react-router";
 
 const Info = () => {
-    return(
-        <React.Fragment>
-            <InfoCard/>
-        </React.Fragment>
-    )
-}
+  const { loading, error, sendRequest, clearError } = useHttpClient();
+  const blogId = useParams().blogId;
+  useEffect(() => {
+    console.log(blogId);
+    const fetchBlog = async () => {
+      try {
+        const responseData = await sendRequest(
+          `http://localhost:5000/api/blogs/${blogId}`
+        );
+      } catch (err) {}
+    };
+    fetchBlog();
+  }, [sendRequest]);
+
+
+  return (
+    <React.Fragment>
+      <ErrorModal error={error} onClear={clearError} />
+      {loading && (
+        <div className="center">
+          <Spinner />
+        </div>
+      )}
+      {!loading && <InfoCard />}
+    </React.Fragment>
+  );
+};
 
 export default Info;
